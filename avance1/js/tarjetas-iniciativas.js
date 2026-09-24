e8b42a181b0b927706a2876367a76fec407cf394
verbrannt-bob <172117560+verbrannt-bob@users.noreply.github.com>
2026-09-24 01:49:50 -0600

    Completar creacion automatica de tarjetas de iniciativas para el catalogo

--- avance1/js/tarjetas-iniciativas.js
+++ avance1/js/tarjetas-iniciativas.js
@@ -8,10 +8,36 @@ function mostrarTarjetas(iniciativas) {
 
     iniciativas.forEach(iniciativa => {
         const tarjeta = document.createElement("article");
+        const competenciasHTML = (iniciativa.competencias || [])
+            .slice(0, 3)
+            .map(competencia => `<p>${competencia}</p>`)
+            .join("");
 
-        tarjeta.className = "col-4"
+        tarjeta.className = "col-11 tarjeta-iniciativa d-flex flex-column gap-1"
         tarjeta.innerHTML = `
-            <h3>${iniciativa.titulo}</h3>
+                <div class="d-flex justify-content-between gap-3 encabezado-tarjeta">
+                    <h3>${iniciativa.titulo}</h3>
+                    <p>${iniciativa.tipo}</p>
+                </div>
+                <div class="resumen">
+                    <p>${iniciativa.resumen}</p>
+                </div>
+                <div class="categoria d-flex justify-content-between">
+                    <p class="etiqueta">Categoria: </p>
+                    <p>${iniciativa.categoria}</p>
+                </div>
+                <div class="propietario d-flex justify-content-between">
+                    <p class="etiqueta">Propietario: </p>
+                    <p>${iniciativa.propietario}</p>
+                </div>
+                <div class="estado d-flex justify-content-between">
+                    <p class="etiqueta">Estado:</p>
+                    <p>${iniciativa.estado}</p>
+                </div>
+                <div class="competencias d-flex flex-column">
+                    <p class="etiqueta">Competencias Requeridas: </p>
+                    ${competenciasHTML}
+                </div>
         `
 
         contenedor.appendChild(tarjeta);

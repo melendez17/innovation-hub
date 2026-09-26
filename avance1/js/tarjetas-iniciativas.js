1fa3f1a70bafdc036d042a5af207d975e6dc0ff2
verbrannt-bob <172117560+verbrannt-bob@users.noreply.github.com>
2026-09-25 23:52:38 -0600

    Agregar funcionalidad de filtros de busqueda y categoria en el catalogo

--- avance1/js/tarjetas-iniciativas.js
+++ avance1/js/tarjetas-iniciativas.js
@@ -1,7 +1,36 @@
-const contenedor = document.querySelector(".contenerdor-tarjetas");
-import iniciativas from '../datos/iniciativas.json' with {type: 'json'};
+const contenedor = document.querySelector(".contenedor-tarjetas");
+let iniciativas;
+const buscador = document.getElementById("buscar");
+const contenedorCategoriasActivas = document.getElementById("contenedor-filtros-activos");
+const categorias = document.querySelectorAll(".categoria");
+const listaCategorias = document.querySelector(".filtro-categorias");
+let categoriasActivas = [];
+let categoriasSeleccionadas = [];
+const idCategorias = new Map();
+idCategorias.set("Tecnología", "tecnologia");
+idCategorias.set("Educación", "educacion");
+idCategorias.set("Sostenibilidad", "sostenibilidad");
+idCategorias.set("Impacto Social", "social");
+idCategorias.set("Negocios y Emprendimiento", "negocios");
+idCategorias.set("Salud y Bienestar", "salud");
+idCategorias.set("Cultura y Creatividad", "cultura");
+idCategorias.set("Otros", "otros");
 
-console.log(iniciativas);
+async function cargarTarjetas() {
+    try {
+        const response = await fetch("../datos/iniciativas.json");
+        if (!response.ok) {
+            throw new Error("Error al obtener las iniciativas");
+        }
+
+        iniciativas = await response.json();
+        console.log(iniciativas);
+        mostrarTarjetas(iniciativas);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
 
 function mostrarTarjetas(iniciativas) {
     contenedor.innerHTML = "";
@@ -44,4 +73,59 @@ function mostrarTarjetas(iniciativas) {
     });
 }
 
-mostrarTarjetas(iniciativas);
+function filtarIniciativas() {
+    const texto = buscador.value.toLowerCase().trim();
+
+    const iniciativasFiltradas = iniciativas.filter(iniciativa => {
+        const resultadoBusqueda = iniciativa.titulo.toLowerCase().includes(texto);
+
+        const coincideCategoria =
+            categoriasSeleccionadas.length === 0 || categoriasSeleccionadas.includes(iniciativa.categoria);
+
+        return resultadoBusqueda && coincideCategoria;
+    })
+
+    mostrarTarjetas(iniciativasFiltradas);
+}
+
+categorias.forEach(categoria => {
+    categoria.addEventListener("click", () => {
+        if (!categoria.classList.contains("activo")) {
+            categoriasSeleccionadas.push(categoria.textContent);
+            categoria.classList.add("activo");
+
+            const filtroActivo = document.createElement("div");
+            filtroActivo.className = "alert alert-primary alert-dismissible fade show col-5 p-1 m-0 d-flex gap-1 justify-content-between align-items-center categoriaActiva"
+            filtroActivo.innerHTML = `
+                <p class="lh-1 m-0">${categoria.textContent}</p>
+                <button type="button" class="btn-close p-0 top-50 translate-middle" data-bs-dismiss="alert" aria-label="Close"></button>
+            `
+            contenedorCategoriasActivas.appendChild(filtroActivo);
+
+            categoriasActivas = document.querySelectorAll(".categoriaActiva");
+            botonEventListener();
+
+
+            filtarIniciativas();
+        }
+    })
+})
+
+buscador.addEventListener("input", () => {
+    filtarIniciativas();
+})
+
+function botonEventListener() {
+    categoriasActivas.forEach(categoria => {
+        categoria.querySelector(".btn-close").addEventListener("click", () => {
+            console.log("corre evento");
+            const indexCat = categoriasSeleccionadas.indexOf(categoria.textContent);
+            categoriasSeleccionadas.splice(indexCat, 1);
+            const idCat = idCategorias.get(categoria.textContent.trim());
+            document.getElementById(idCat).classList.remove("activo");
+            filtarIniciativas();
+        })
+    })
+}
+
+cargarTarjetas();
